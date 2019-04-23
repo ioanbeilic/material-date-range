e2ab5db431cf2b6aac58a1b4b399c58425589aef
ioan <ioan@mbp-de-ioan.home>
2019-04-24 00:01:17 +0200

    event test

--- projects/date-range-picker/src/lib/date-range-picker.component.ts
+++ projects/date-range-picker/src/lib/date-range-picker.component.ts
@@ -2,13 +2,7 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "lib-date-range-picker",
-  template: `
-    <mat-form-field (click)="picker.open()" class="example-full-width">
-      <input matInput [matDatepicker]="picker" placeholder="Choose a date" />
-      <mat-datepicker-toggle matSuffix [for]="picker"></mat-datepicker-toggle>
-      <mat-datepicker touchUi #picker></mat-datepicker>
-    </mat-form-field>
-  `,
+  template: ``,
   styles: []
 })
 export class DateRangePickerComponent implements OnInit {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'material-date-range';
+  title = "material-date-range";
+  from: any;
+  to: any;
+
+  test(e) {
+    console.log("test");
+    console.log(e);
+    console.log(this.from);
+    console.log(this.to);
+  }
 }
